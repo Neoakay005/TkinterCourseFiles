2c7ed2aa45089ca0ba90e8d6a70fc646458a967d
Neoakay005 <akayhalil0505@gmail.com>
2024-11-28 17:59:26 +0300

    buttons added

--- main.py
+++ main.py
@@ -1,13 +1,54 @@
+from statistics import variance
 import tkinter as tk
-from tkinter import ttk
+from tkinter import BUTT, StringVar, ttk
 import ttkbootstrap as ttk
 
 
 # window setup
-window = ttk.Window(themename = 'solar')
+window = ttk.Window(themename = 'yeti')
 window.title("Buttons")
 window.geometry("400x200")
 # -------------------------------
+# Basic Buttons -----------------
+# function
+def button_func():
+    print("basic button clicked!")
+
+button_string = StringVar(value = "Cklick!")
+button = ttk.Button(window, text="A simple button", command=button_func, textvariable=button_string)
+button.pack(pady=10)
+
+
+# Basic Checkbutton -------------
+# store value
+check_var = ttk.IntVar(value = 10)  # for checked in dafault
+check = ttk.Checkbutton(
+    window, 
+    text="checkbox 1", 
+    command=lambda: print(f"checkbutton 1 clicked! Value: {check_var.get()}"),
+    variable=check_var,
+    onvalue= 10,
+    offvalue= -10)
+check.pack(pady=10)
+
+# Radio buttons -----------------
+
+radio_var = ttk.StringVar()
+radio1 = ttk.Radiobutton(
+    window, 
+    text = "Radio Button 1", 
+    value="radio1", 
+    variable=radio_var, 
+    command=lambda: print(radio_var.get()))
+radio1.pack()
+
+radio2 = ttk.Radiobutton(
+    window, 
+    text = "Radio Button 2", 
+    value="radio2", 
+    variable=radio_var, 
+    command=lambda: print(radio_var.get()))
+radio2.pack()
 
 
 # -------------------------------
